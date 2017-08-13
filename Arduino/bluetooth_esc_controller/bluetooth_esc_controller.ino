c2e85383f312a041e51de652552aac5169ac5b35
Unknown <karim.shehata1@gmail.com>
2017-08-13 20:23:17 +0200

    Created a Arduino Project and Solution.
    Added Code for communicating with the servo/esc.

--- Arduino/bluetooth_esc_controller/bluetooth_esc_controller.ino
+++ Arduino/bluetooth_esc_controller/bluetooth_esc_controller.ino
@@ -1,24 +1,51 @@
 #include <SoftwareSerial.h>
+#include <Servo.h>
 
 //SoftwareSerial to keep Serial for Servo communication free
 SoftwareSerial BTSerial(8, 9); // RX | TX on Arduino. RX(Arduino) -> TX(HC-06). TX(Arduino) -> RX(HC-06)
 
+Servo servo;  // create servo object to control a servo
+
+String message = "";
+
+int minSpeed = 0;  //min speed
+int maxSpeed = 180; //max effective speed = 90
+
 void setup()
 {
-    Serial.begin(9600);   //Sets the baud for serial data transmission                               
-    pinMode(13, OUTPUT);  //Sets digital pin 13 as output pin
+	Serial.begin(9600);   //Sets the baud for serial data transmission                               
+	pinMode(13, OUTPUT);  //Sets digital pin 13 as output pin
 
-    BTSerial.begin(9600); // HC-06 current baud rate (default 115200)
+	BTSerial.begin(9600); // HC-06 current baud rate (default 115200)
+
+	servo.attach(14);  // attaches the servo on pin 9 to the servo object
+
+	servo.write(0); // arm esc
+	delay(3000);
 }
+
 void loop()
 {
-  if (BTSerial.available())
-  {
-     char c = BTSerial.read();
 
-    if(c == ';')
-      Serial.println();
-    else
-      Serial.write(c);
-  }
-}^
+	if (BTSerial.available())
+	{
+		char c = BTSerial.read();
+
+		if (c == ';')
+		{
+			int i = message.toInt();
+
+			int val = map(i, 0, 100, minSpeed, maxSpeed);     // scale it to use it with the servo (value between 0 and 180)
+			if (i == 0)
+				servo.write(0);
+			else
+				servo.write(val);
+
+			message = "";
+		}
+		else
+		{
+			message += c;
+		}
+	}
+}
